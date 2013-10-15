4ff4ef6e1017e0c1170285efcef364151e5b40f5
cis <cis@CISM424.(none)>
2013-10-15 15:37:53 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DubeyClass::Application.config.secret_token = 'feb0d7330ee8274e626c611b3d03535c7730b8b3d7d69488ea284ff688c1ca186bbfbef55d1362ef194941d5518a65f5d185750e24833e0cb845ff2cf4de3eef'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+DubeyClass::Application.config.session_store :cookie_store, key: '_dubey-class_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# DubeyClass::Application.config.session_store :active_record_store
